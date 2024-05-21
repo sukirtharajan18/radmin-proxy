0173c16ac39c31c6dfb1ca65d0b0ca9f4a71bd5d
sukirtharajan18 <sukirtharajan6@gmail.com>
2024-05-21 20:40:29 +0530

    added system shutdown route

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const http = require("http");
 const httpProxy = require("http-proxy");
+const { exec } = require("child_process");
 
 const proxy = httpProxy.createProxyServer({});
 
 const target = "http://radmin-ip:8096";
 
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target }, (error) => {
-    console.error("Proxy error:", error);
-    res.writeHead(502);
-    res.end("Bad Gateway");
-  });
+  if (req.url === "/shutdown") {
+    exec("shutdown /s /f /t 0", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Shutdown error: ${error}`);
+        res.writeHead(500);
+        res.end("Internal Server Error");
+        return;
+      }
+      console.log("Shutdown command executed");
+      res.writeHead(200);
+      res.end("System is shutting down");
+    });
+  } else {
+    proxy.web(req, res, { target }, (error) => {
+      console.error("Proxy error:", error);
+      res.writeHead(502);
+      res.end("Bad Gateway");
+    });
+  }
 });
 
 const port = 8080;
